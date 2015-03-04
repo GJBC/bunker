5c5860005bc20730f1d0284f8d3bef84f00451dc
Zzortell <remi.blaise@gmx.fr>
2015-03-04 20:19:15 +0100

    Add js to ex-html

---
+++ ex-html/js/script.js
@@ -0,0 +1,11 @@
+switch ( prompt ('Qu\'aimes-tu ?') ) {
+	case 'camembert' :
+		console.log('Formidable !!');
+	break;
+	case 'mangue' :
+		console.log('Youpi !');
+	default:
+		console.log('Rien ...');
+}
+
+console.log ('Hey !');
