c84b03304620882a42add35def188080a421da3d
Rémi Blaise <remi.blaise@gmx.fr>
2015-05-20 17:11:34 +0200

    Ajout de marchand de glaces

---
+++ glaces/marchand_de_glaces.js
@@ -0,0 +1,12 @@
+	var yourglace = document.createElement('p');
+	var image = document.createElement('img');
+	image.src = 'http://sf1.viepratique.fr/wp-content/uploads/sites/2/2014/04/shutterstock_138429458.jpg';
+	yourglace.appendChild(image);
+do {
+	var yourglace = document.createElement('p');
+	var image = document.createElement('img');
+	image.src = 'http://sf1.viepratique.fr/wp-content/uploads/sites/2/2014/04/shutterstock_138429458.jpg';
+	yourglace.appendChild(image);
+	
+	var conf = confirm ( 'Veux-tu une glace ?');
+} while (conf == true)
