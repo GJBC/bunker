efcb413f8287df71c93c71e797162660af91f9a4
Rémi Blaise <remi.blaise@gmx.fr>
2015-05-21 18:45:25 +0200

    Cours sur les events

--- ex-html/js/script.js
+++ ex-html/js/script.js
@@ -1,18 +1,11 @@
-// Je récupère l'objet html
-var body = document.getElementsByTagName('body')[0];
+var container = document.getElementById('container'),
+	inte = document.getElementById('inte'),
+	field = document.getElementById('field'),
+	link = document.getElementById('a');
 
-// Je crée mon nouveau paragraphe
-var myParagraph = document.createElement('p');
-// Je lui rajoute des attributs
-myParagraph.id = 'myId';
-myParagraph.title = 'Infobulle !';
-
-// Je crée un noeud textuel
-var noeudText = document.createTextNode('#text');
-// Je lui définis un du texte
-noeudText.nodeValue = 'Je suis un paragraphe ajouté dynamiquement en Js !';
-// J'insère mon noeud textuel
-myParagraph.appendChild(noeudText);
-
-// Je l'insère
-body.appendChild(myParagraph);
+container.addEventListener('click', function ( e ) {
+	
+	e.preventDefault();
+	
+	console.log('hey !');
+});
